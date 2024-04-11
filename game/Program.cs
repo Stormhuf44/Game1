3413f6e5e4c4a1fd8335f4c8f4afd71a7a7df625
cc221052 <cc221052@fhstp.ac.at>
2024-04-11 19:02:50 +0200

    Movement Refactor and figure adding

--- game/Program.cs
+++ game/Program.cs
@@ -15,36 +15,9 @@ class Program
             GameEngine.Render();
 
             // Handle keyboard input
-            ConsoleKeyInfo keyInfo = Console.ReadKey(true);
-            HandleInput(keyInfo);
+            //ConsoleKeyInfo keyInfo = Console.ReadKey(true);
+            InputHandler.RegisterInputHandler();
+            
         }
-    }
-
-    static void HandleInput(ConsoleKeyInfo keyInfo)
-    {
-
-        GameObject focusedObject = GameEngine.GetFocusedObject();
-
-        if (focusedObject != null) {
-            // Handle keyboard input to move the player
-            switch (keyInfo.Key)
-            {
-                case ConsoleKey.UpArrow:
-                    focusedObject.Move(0, -1);
-                    break;
-                case ConsoleKey.DownArrow:
-                    focusedObject.Move(0, 1);
-                    break;
-                case ConsoleKey.LeftArrow:
-                    focusedObject.Move(-1, 0);
-                    break;
-                case ConsoleKey.RightArrow:
-                    focusedObject.Move(1, 0);
-                    break;
-                default:
-                    break;
-            }
-        }
-        
     }
 }
